82afaa16c1aec462f1e1310ab8b25feaf6fabd7f
PaulArias27 <paularias2727@gmail.com>
2026-05-16 22:36:31 -0500

    PA: se crearon las clases hijas y se creo el nuevo paquete testcon la clase testFiguras

---
+++ src/com/krakedev/test/TestFiguras.java
@@ -0,0 +1,19 @@
+package com.krakedev.test;
+
+import com.krakedev.figuras.Cuadrado;
+import com.krakedev.figuras.Figura;
+import com.krakedev.figuras.Triangulo;
+
+public class TestFiguras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Figura f = new Figura();
+		Cuadrado c = new Cuadrado();
+		Triangulo t = new Triangulo();
+		
+		System.out.println(t);
+
+	}
+
+}
